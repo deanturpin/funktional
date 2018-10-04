270bdf97690f86bab5955145507cd3a6d55223ef
deanturpin <kali@brightcoin.uk>
2018-10-04 12:09:33 +0100

    Add more static assertions and usage notes

--- vektor.h
+++ vektor.h
@@ -12,6 +12,12 @@ template <typename T = double> struct vektor : public std::vector<T> {
   // A constructor with a value pushes the value onto the empty vector
   vektor(const T &a) { this->push_back(a); }
 
+  // std::vector has non-virtual destructors so we can't do this
+  // virtual ~vektor() = default;
+
+  // Nor can we add members that might not be destroyed properly
+  // int blah;
+
   // Construct with a std::vector
   vektor(const std::vector<T> &a) {
     this->reserve(a.size());
@@ -42,7 +48,18 @@ template <typename T = double> struct vektor : public std::vector<T> {
   }
 };
 
-static_assert(std::is_standard_layout<vektor<double>>::value);
+using _vek = vektor<double>;
+static_assert(std::is_standard_layout<_vek>::value);
+
+static_assert(std::is_constructible<_vek>::value);
+static_assert(std::is_default_constructible<_vek>::value);
+static_assert(std::is_copy_constructible<_vek>::value);
+static_assert(std::is_move_constructible<_vek>::value);
+static_assert(std::is_assignable<_vek, std::vector<double>>::value);
+static_assert(std::is_assignable<std::vector<double>, _vek>::value);
+static_assert(std::is_move_assignable<_vek>::value);
+static_assert(std::is_destructible<_vek>::value);
+static_assert(!std::has_virtual_destructor<_vek>::value);
 
 } // namespace vek
 
