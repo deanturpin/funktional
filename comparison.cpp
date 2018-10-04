42b838e598c2c9feff26617c830218e55572a353
deanturpin <kali@brightcoin.uk>
2018-10-04 18:29:17 +0100

    Add vektor example

--- comparison.cpp
+++ comparison.cpp
@@ -7,20 +7,46 @@ std::string generate(const int n) {
   return n > 1 ? generate(n - 1) + std::to_string(n - 1) : "0";
 }
 
+void strings(const int n) {
+
+  // Iterative
+  std::string iterative;
+  for (int i = 0; i < n; ++i)
+    iterative += std::to_string(i);
+
+  // Recursive
+  const std::string recursive = generate(n);
+
+  for (const auto &y : {iterative, recursive})
+    std::cout << y << '\n';
+
+  assert(iterative == recursive);
+}
+
+vek::vektor<double> generate_vector(const int n) {
+  return n > 0 ? generate_vector(n - 1) + n : 0;
+}
+
+void vectors(const int n) {
+
+  // Iterative
+  std::vector<double> iterative;
+  for (int i = 0; i < n; ++i)
+    iterative.push_back(i + 1);
+
+  // Recursive
+  vek::vektor<double> recursive = generate_vector(n);
+
+  for (const auto &y : {iterative, std::vector<double>(recursive)})
+    for (const auto &x : y)
+      std::cout << x << '\n';
+
+  assert(iterative == recursive);
+}
+
 int main() {
 
   const int n = 10;
-
-  // Iterative approach
-  std::string a;
-  for (int i = 0; i < n; ++i)
-    a += std::to_string(i);
-
-  // Functional approach using recursion
-  const std::string b = generate(n);
-
-  for (const auto &x : {a, b})
-    std::cout << x << '\n';
-
-  assert(a == b);
+  strings(n);
+  vectors(n);
 }
